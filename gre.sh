e4aa590b8dd3e28e1a4e7b64666853b0e1270172
abrdade <abrdadecom@gmail.com>
2025-07-07 16:40:54 +0330

    Update gre.sh

--- gre.sh
+++ gre.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-
+echo ""
+echo "===================================="
+echo "        GitHub: AbrDade"
+echo "   GRE Tunnel Setup Script"
+echo "===================================="
+echo ""
 echo "Select server location:"
 echo "1 - IRAN"
 echo "2 - FOREIGN"
